aa5e955f0113d93e47c0db19b08aec623892d032
lixin <lixin@karrytech.com>
2023-12-19 17:16:55 +0800

    [FIX]

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 
 cd ./app
 
-git clone -b 17.0 https://github.com/odoo/odoo.git
+folder_path="odoo"
+
+if [ ! -d "$folder_path" ]; then
+  git clone -b 17.0 https://github.com/odoo/odoo.git
+fi
 
 docker-compose up -d
